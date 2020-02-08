c5e806333f4a292896b2cf2ae549e9a6770a0dad
Gustavo <gustavomendes.medeiros@gmail.com>
2020-02-08 18:33:06 -0300

    Create UserController and validators

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,14 @@
-import { Router } from "express";
+import { Router } from 'express';
+
+import UserController from './app/controllers/UserController';
+
+import validateUserStore from './app/validators/UserStore';
+import validateUserUpdate from './app/validators/UserUpdate';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => res.json("Hello World"));
+routes.get('/', (req, res) => res.json('Hello World'));
+
+routes.post('/users', validateUserStore, UserController);
 
 export default routes;
